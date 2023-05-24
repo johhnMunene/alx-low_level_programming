5cab98a7d90d600750f9dd7fcb4acc3c87e62ef7
jo-svg <edwardsjohnny64@gmail.com>
2023-05-24 05:43:47 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	_putchar(ch);
+	}
+
+	_putchar('\n');
+
+}
+
+
