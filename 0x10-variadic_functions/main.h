39b6247096d3c23f69f6ecfabf611ac96408d7e6
johhnmunene <edwardsjohnny64@gmail.com>
2023-06-23 01:41:57 +0300

    -sum_them_all

---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+ void print_numbers(const char *separator, const unsigned int n, ...);
+ void print_strings(const char *separator, const unsigned int n, ...);
+#endif
