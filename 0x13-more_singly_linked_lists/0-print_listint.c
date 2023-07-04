b253b5fa3b942e5aa4fd9319cbcf8c939f980873
johhnmunene <edwardsjohnny64@gmail.com>
2023-07-04 16:53:53 +0300

    error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,24 +1,21 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a linked dlistint_t list.
- * @h: pointer to the head of the list
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
  *
- * Return: number of nodes
+ * Return: the number of nodes
  */
-
-size_t dlistint_len(const dlistint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	int counter = 0;
+const listint_t *cursor = h;
+size_t count = 0;
 
-	if (h == NULL)
-		return (counter);
-
-	while (h != NULL)
-	{
-		counter++;
-		h = h->next;
-	}
-
-	return (counter);
+while (cursor != NULL)
+{
+printf("%d\n", cursor->n);
+count += 1;
+cursor = cursor->next;
+}
+return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -6,6 +6,13 @@
 #include <string.h>
 #include <stdio.h>
 
+typedef struct listint_s
+{
+int n;
+struct listint_s *next;
+} listint_t;
+
+
 size_t print_listint(const listint_t *h);
 
 
